cbc6f4c195c94279e4fef71d8c7f0a655c592b57
vapcomm <68540035+vapcomm@users.noreply.github.com>
2023-09-15 14:13:58 +0400

    #21 Decode the Morse code, advanced

--- app/src/main/java/online/vapcom/codewars/codes/DecodeTheMorseCode.kt
+++ app/src/main/java/online/vapcom/codewars/codes/DecodeTheMorseCode.kt
@@ -1,14 +1,15 @@
 package online.vapcom.codewars.codes
 
-val MorseCode : HashMap<String, String> = hashMapOf(
+val MORSE_CODE : HashMap<String, String> = hashMapOf(
     "...---..." to "SOS",
-    // "HEY JUDE", decodeMorse(".... . -.--   .--- ..- -.. ."
-    "...." to "H",
-    "." to "E",
-    "-.--" to "Y",
-    ".---" to "J",
-    "..-" to "U",
-    "-.." to "D",
+   	".-" to "A", "-..." to "B",	"-.-." to "C", "-.." to "D",
+  	"." to "E", "..-." to "F", "--." to "G", "...." to "H",
+  	".." to "I", ".---" to "J", "-.-" to "K", ".-.." to "L",
+  	"--" to "M", "-." to "N", "---" to "O",	".--." to "P",
+ 	"--.-" to "Q", ".-." to "R", "..." to "S", "-" to "T",
+ 	"..-" to "U", "...-" to "V", ".--" to "W", "-..-" to "X",
+ 	"-.--" to "Y", "--.." to "Z",
+    ".-.-.-" to "."
 )
 
 fun decodeMorse(code: String): String {
@@ -17,10 +18,144 @@ fun decodeMorse(code: String): String {
     code.trim().split("   ").forEach { word ->
         word.split(" ").forEach { letter ->
             if(letter.isNotEmpty())
-                sb.append(MorseCode[letter] ?: "_")
+                sb.append(MORSE_CODE[letter] ?: "_")
         }
         sb.append(" ")
     }
 
     return sb.toString().trim()
+}
+
+/**
+ * Decode the Morse code, advanced
+ *
+ * https://www.codewars.com/kata/54b72c16cd7f5154e9000457/train/kotlin
+ *
+ * Decode string of 0s and 1s to Morse code
+ *```
+ * 1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011
+ * ···· · −·−−   ·−−− ··− −·· ·
+ *```
+ * "Dot" – is 1 time unit long.
+ * "Dash" – is 3 time units long.
+ * Pause between dots and dashes in a character – is 1 time unit long.
+ * Pause between characters inside a word – is 3 time units long.
+ * Pause between words – is 7 time units long.
+ *
+ * Note that some extra 0's may naturally occur at the beginning and the end of a message, make sure to ignore them.
+ * Also if you have trouble discerning if the particular sequence of 1's is a dot or a dash, assume it's a dot.
+ */
+fun decodeBits(bits: String): String {
+
+    // return number of zeros from offset
+    fun skipZeros(bits: String, off: Int): Int {
+        var i = off
+        while (i < bits.length && bits[i] == '0') i++
+
+        return i - off
+    }
+
+    // return string with spaces which length is based on zeros' number
+    fun spacesFromZeros(zeros: Int, timeUnit: Int): String {
+        return when {
+            // pause between characters
+            timeUnit * 2 < zeros && zeros < timeUnit * 7 -> " "
+            // pause between words
+            zeros >= timeUnit * 7 -> "   "
+            else -> ""
+        }
+    }
+
+    // return minimum '1' sequence length, i.e. time unit
+    fun findTimeUnit(bits: String): Int {
+        val trimmedBits = bits.trim('0')
+        if (trimmedBits.isEmpty()) return 0
+
+        var currentChar = trimmedBits[0]
+        var sequenceLength = 0
+        var minSequence = Int.MAX_VALUE
+
+        trimmedBits.forEach { c ->
+            if (c == currentChar) {
+                sequenceLength++
+            } else {
+                if (sequenceLength < minSequence)
+                    minSequence = sequenceLength
+
+                if (minSequence <= 1) return 1      // it's a minimum possible time unit
+
+                currentChar = c
+                sequenceLength = 1
+            }
+        }
+
+        // last sequence in bits
+        if (sequenceLength < minSequence)
+            minSequence = sequenceLength
+
+        return minSequence
+    }
+
+    // println("-- bits: '$bits'")
+
+    val timeUnit = findTimeUnit(bits)
+
+    if (timeUnit <= 0) return ""
+
+    val sb = StringBuilder()
+    var n = skipZeros(bits, 0)
+    var state = BitsDecoderState.DOT
+    var symbolLength = 0
+
+    while (n < bits.length) {
+        symbolLength++
+
+        when (state) {
+            BitsDecoderState.DOT -> {
+                if (bits[n] == '1') {
+                    if (symbolLength > timeUnit * 2)
+                        state = BitsDecoderState.DASH
+
+                    n++
+                } else {
+                    sb.append('.')
+                    symbolLength = 0
+
+                    val zeros = skipZeros(bits, n)
+                    val spaces = spacesFromZeros(zeros, timeUnit)
+                    if (spaces.isNotEmpty()) sb.append(spaces)
+                    n += zeros
+                }
+            }
+            BitsDecoderState.DASH -> {
+                if (bits[n] == '1') {
+                    n++
+                } else {
+                    if (symbolLength >= timeUnit * 3) sb.append('-')
+                    else sb.append('.')
+
+                    symbolLength = 0
+                    state = BitsDecoderState.DOT
+
+                    val zeros = skipZeros(bits, n)
+                    val spaces = spacesFromZeros(zeros, timeUnit)
+                    if (spaces.isNotEmpty()) sb.append(spaces)
+                    n += zeros
+                }
+            }
+        }
+    }
+
+    // possible last symbol in bits
+    if (symbolLength >= timeUnit * 3) sb.append('-')
+    else if (symbolLength >= 1) sb.append('.')
+
+    // println("-- result: '$sb'")
+
+    return sb.toString()
+}
+
+
+enum class BitsDecoderState {
+    DOT, DASH
 }
--- app/src/test/java/online/vapcom/codewars/codes/DecodeTheMorseCodeTest.kt
+++ app/src/test/java/online/vapcom/codewars/codes/DecodeTheMorseCodeTest.kt
@@ -28,4 +28,51 @@ class DecodeTheMorseCodeTest {
         assertEquals("HEY JUDE", decodeMorse(".... . -.--   .--- ..- -.. ."))
     }
 
+    /**
+     * Decode the Morse code, advanced
+     *
+     * https://www.codewars.com/kata/54b72c16cd7f5154e9000457/train/kotlin
+     */
+    @Test
+    fun decodeBitsExampleTestCases() {
+        assertEquals("HEY JUDE", decodeMorse(decodeBits("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")))
+
+        // testBasicBitsDecoding
+        assertEquals("E", decodeMorse(decodeBits("1")))
+        assertEquals("I", decodeMorse(decodeBits("101")))
+        assertEquals("EE", decodeMorse(decodeBits("10001")))
+        assertEquals("A", decodeMorse(decodeBits("10111")))
+        assertEquals("M", decodeMorse(decodeBits("1110111")))
+
+        // testLongMessagesHandling
+        assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.", decodeMorse(decodeBits("00011100010101010001000000011101110101110001010111000101000111010111010001110101110000000111010101000101110100011101110111000101110111000111010000000101011101000111011101110001110101011100000001011101110111000101011100011101110001011101110100010101000000011101110111000101010111000100010111010000000111000101010100010000000101110101000101110001110111010100011101011101110000000111010100011101110111000111011101000101110101110101110")))
+
+        // testMultipleBitsPerDotHandling
+        assertEquals("E", decodeMorse(decodeBits("111")))
+        assertEquals("E", decodeMorse(decodeBits("1111111")))
+        assertEquals("I", decodeMorse(decodeBits("110011")))
+        assertEquals("I", decodeMorse(decodeBits("111000111")))
+        assertEquals("I", decodeMorse(decodeBits("111110000011111")))
+        assertEquals("M", decodeMorse(decodeBits("11111100111111")))
+
+        // testExtraZerosHandling
+        assertEquals("E", decodeMorse(decodeBits("01110")))
+    }
+
+    @Test
+    fun decodeBitsTestCases() {
+        assertEquals("", decodeBits(""))
+        assertEquals("", decodeBits("0"))
+        assertEquals("", decodeBits("00000"))
+
+        assertEquals(".", decodeBits("1"))
+
+        assertEquals(".", decodeBits("11"))
+        assertEquals(".", decodeBits("0011"))
+        assertEquals("..", decodeBits("110011"))
+        assertEquals("..", decodeBits("11001100"))
+
+        assertEquals("...", decodeBits("110011001"))
+    }
+
 }
