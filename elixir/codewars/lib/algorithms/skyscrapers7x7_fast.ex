cc9a0285ea84e3ae32417c8f85cec0bfc02f8f26
vapcomm <68540035+vapcomm@users.noreply.github.com>
2025-01-16 18:22:15 +0400

    Elixir #9 - 7 By 7 Skyscrapers. Made fast substractive variant, only attempt "medved" test failed.

--- elixir/codewars/lib/algorithms/skyscrapers7x7_fast.ex
+++ elixir/codewars/lib/algorithms/skyscrapers7x7_fast.ex
@@ -1,17 +1,17 @@
-defmodule Skyscrapers4x4Fast do
+defmodule Skyscrapers7x7Fast do
   @moduledoc """
-  4 By 4 Skyscrapers -- 4 kyu
-  https://www.codewars.com/kata/5671d975d81d6c1c87000022/train/elixir
+  7 By 7 Skyscrapers -- 1 kyu
+  https://www.codewars.com/kata/5917a2205ffc30ec3a0000a8/train/elixir
 
-  **NOTE:** this is fast solution version based on ideas of farhadi solution.
-  Instead of the first "additive" algorithm, it uses "subtractive" search of common cells in grid.
+  This is a fast "subtractive" variant of the solution, it uses ideas from farhadi hints intersection algorithm.
+  See Skyscrapers4x4Fast and tests for details.
   """
 
   import Bitwise
 
   @doc """
-  Pass the clues in an array of 16 items. This array contains the clues around the clock.
-  Return 4x4 matrix, list of rows of ints.
+  Pass the clues in an array of 28 items. This array contains the clues around the clock.
+  Return 7x7 matrix, list of rows of ints.
   """
   def solve(clues) do
     {rows_hints, columns_hints} = make_hints(clues)
@@ -21,16 +21,19 @@ defmodule Skyscrapers4x4Fast do
 
   @doc """
   Return tuple with two lists with permutations lists, first for rows, second for columns:
-  {[[{},...], [{},...], [{},...], [{},...]], [[{},...], [{},...], [{},...], [{},...]]}
-  Example for a real hints see in unit tests.
+  first for rows, second for columns:
+  {[[{},...], [{},...], ...], [[{},...], [{},...], ...]}
   Clues in given list form the circle around a grid:
   ```
-      0  1  2  3
-  15| 0  0  0  0 | 4
-  14| 0  0  0  0 | 5
-  13| 0  0  0  0 | 6
-  12| 0  0  0  0 | 7
-     11 10  9  8
+      0  1  2  3  4  5  6
+  27| 0  0  0  0  0  0  0| 7
+  26| 0  0  0  0  0  0  0| 8
+  25| 0  0  0  0  0  0  0| 9
+  24| 0  0  0  0  0  0  0| 10
+  23| 0  0  0  0  0  0  0| 11
+  22| 0  0  0  0  0  0  0| 12
+  21| 0  0  0  0  0  0  0| 13
+     20 19 18 17 16 15 14
   ```
   """
   def make_hints(clues) do
@@ -38,17 +41,23 @@ defmodule Skyscrapers4x4Fast do
     perms = make_permutations_table()
 
     rows_hints = [
-      perms[clues_to_key(elem(cl, 15), elem(cl, 4))],
-      perms[clues_to_key(elem(cl, 14), elem(cl, 5))],
-      perms[clues_to_key(elem(cl, 13), elem(cl, 6))],
-      perms[clues_to_key(elem(cl, 12), elem(cl, 7))]
+      perms[clues_to_key(elem(cl, 27), elem(cl, 7))],
+      perms[clues_to_key(elem(cl, 26), elem(cl, 8))],
+      perms[clues_to_key(elem(cl, 25), elem(cl, 9))],
+      perms[clues_to_key(elem(cl, 24), elem(cl, 10))],
+      perms[clues_to_key(elem(cl, 23), elem(cl, 11))],
+      perms[clues_to_key(elem(cl, 22), elem(cl, 12))],
+      perms[clues_to_key(elem(cl, 21), elem(cl, 13))]
     ]
 
     columns_hints = [
-      perms[clues_to_key(elem(cl, 0), elem(cl, 11))],
-      perms[clues_to_key(elem(cl, 1), elem(cl, 10))],
-      perms[clues_to_key(elem(cl, 2), elem(cl, 9))],
-      perms[clues_to_key(elem(cl, 3), elem(cl, 8))]
+      perms[clues_to_key(elem(cl, 0), elem(cl, 20))],
+      perms[clues_to_key(elem(cl, 1), elem(cl, 19))],
+      perms[clues_to_key(elem(cl, 2), elem(cl, 18))],
+      perms[clues_to_key(elem(cl, 3), elem(cl, 17))],
+      perms[clues_to_key(elem(cl, 4), elem(cl, 16))],
+      perms[clues_to_key(elem(cl, 5), elem(cl, 15))],
+      perms[clues_to_key(elem(cl, 6), elem(cl, 14))],
     ]
 
     {rows_hints, columns_hints}
@@ -65,14 +74,14 @@ defmodule Skyscrapers4x4Fast do
       left_clue = find_left_visibility(perm)
       right_clue = find_right_visibility(perm)
       key = clues_to_key(left_clue, right_clue)
-      eperm = List.to_tuple(perm)
-      {_, pair_result} = Map.get_and_update(acc, key, fn perms_list -> update_perms_list(perms_list, eperm) end)
+      t_perm = List.to_tuple(perm)
+      {_, pair_result} = Map.get_and_update(acc, key, fn perms_list -> update_perms_list(perms_list, t_perm) end)
 
       left_key = clues_to_key(left_clue, 0)
-      {_, left_result} = Map.get_and_update(pair_result, left_key, fn perms_list -> update_perms_list(perms_list, eperm) end)
+      {_, left_result} = Map.get_and_update(pair_result, left_key, fn perms_list -> update_perms_list(perms_list, t_perm) end)
 
       right_key = clues_to_key(0, right_clue)
-      {_, result} = Map.get_and_update(left_result, right_key, fn perms_list -> update_perms_list(perms_list, eperm) end)
+      {_, result} = Map.get_and_update(left_result, right_key, fn perms_list -> update_perms_list(perms_list, t_perm) end)
 
       result
     end)
@@ -118,12 +127,13 @@ defmodule Skyscrapers4x4Fast do
     result
   end
 
-  def permutations() do
-    [
-      [1, 2, 3, 4], [2, 1, 3, 4], [3, 1, 2, 4], [1, 3, 2, 4], [2, 3, 1, 4], [3, 2, 1, 4], [4, 2, 3, 1], [2, 4, 3, 1],
-      [3, 4, 2, 1], [4, 3, 2, 1], [2, 3, 4, 1], [3, 2, 4, 1], [4, 1, 3, 2], [1, 4, 3, 2], [3, 4, 1, 2], [4, 3, 1, 2],
-      [1, 3, 4, 2], [3, 1, 4, 2], [4, 1, 2, 3], [1, 4, 2, 3], [2, 4, 1, 3], [4, 2, 1, 3], [1, 2, 4, 3], [2, 1, 4, 3]
-    ]
+  @doc """
+  Generate all permutations of 1..7, return list of permutations as numbers lists
+  """
+  def permutations(), do: permutations([1, 2, 3, 4, 5, 6, 7])
+  def permutations([i]), do: [[i]]
+  def permutations(list) do
+    for h <- list, t <- permutations(list -- [h]), do: [h | t]
   end
 
   @doc """
@@ -146,11 +156,14 @@ defmodule Skyscrapers4x4Fast do
   """
   def find_common(rows_hints, columns_hints) do
     # make bitmasks from rows, one masks' row example:
-    # { 0b110, 0b1100, 0b10000, 0b1110}
-    # Each bit in mask is for numbers 1..4, i.e. 0b110 means in this grid's cell numbers 1 or 2 are possible
+    # { 0b110, 0b1100, 0b10000, 0b1110, 0b110000, 0b11111110, 0b10000000}
+    # Each "one" bit in mask is for numbers 1..7, i.e. 0b110 means in this grid's cell numbers 1 or 2 are possible.
     masks = Enum.map(rows_hints, fn row_hints ->
-      Enum.reduce(row_hints, {0, 0, 0, 0}, fn {r0, r1, r2, r3}, {a0, a1, a2, a3} ->
-        {(1 <<< r0) ||| a0, (1 <<< r1) ||| a1, (1 <<< r2) ||| a2, (1 <<< r3) ||| a3}
+      Enum.reduce(row_hints, {0, 0, 0, 0, 0, 0, 0}, fn {r0, r1, r2, r3, r4, r5, r6}, {a0, a1, a2, a3, a4, a5, a6} ->
+        {
+          (1 <<< r0) ||| a0, (1 <<< r1) ||| a1, (1 <<< r2) ||| a2, (1 <<< r3) ||| a3,
+          (1 <<< r4) ||| a4, (1 <<< r5) ||| a5, (1 <<< r6) ||| a6
+        }
       end)
     end) |> List.to_tuple()
 
@@ -158,26 +171,16 @@ defmodule Skyscrapers4x4Fast do
     columns_hints
     |> Enum.with_index()
     |> Enum.map(fn {hints, c} ->
-      Enum.filter(hints, fn {c0, c1, c2, c3} ->
+      Enum.filter(hints, fn {c0, c1, c2, c3, c4, c5, c6} ->
         ((1 <<< c0) &&& elem(elem(masks, 0), c)) != 0 and
         ((1 <<< c1) &&& elem(elem(masks, 1), c)) != 0 and
         ((1 <<< c2) &&& elem(elem(masks, 2), c)) != 0 and
-        ((1 <<< c3) &&& elem(elem(masks, 3), c)) != 0
+        ((1 <<< c3) &&& elem(elem(masks, 3), c)) != 0 and
+        ((1 <<< c4) &&& elem(elem(masks, 4), c)) != 0 and
+        ((1 <<< c5) &&& elem(elem(masks, 5), c)) != 0 and
+        ((1 <<< c6) &&& elem(elem(masks, 6), c)) != 0
       end)
     end)
-  end
-
-  # --------- extra functions, not for kata solution ---------
-  @doc """
-  Converts tuple with hints lists to string for logs
-  """
-  def hints_to_string(hints) do
-    hints
-    |> Enum.with_index()
-    |> Enum.map(fn {row_hints, index} ->
-      "#{index}: #{length(row_hints)}-[#{Enum.join(Enum.map(row_hints, fn h -> inspect(h) end), ", ")}]"
-    end)
-    |> Enum.join("\n")
   end
 
 end
